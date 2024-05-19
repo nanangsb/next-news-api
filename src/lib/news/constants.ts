ff0ae73e5a17ebcdb3e552c35770ff55ffae0076
nanangsb <sefida.nanang@gmail.com>
2024-05-19 13:57:49 +0700

    Update constants.ts

--- src/lib/news/constants.ts
+++ src/lib/news/constants.ts
@@ -1,31 +1,31 @@
 export const newsSources: Source[] = [ 
   {
-    code: "id-muslim",
+    code: "ID-MUSLIM",
     name: "Muslim.or.id",
     url: "https://muslim.or.id/feed",
   },
   {
-    code: "id-muslimah",
+    code: "ID-MUSLIMAH",
     name: "Muslimah.or.id",
     url: "https://muslimah.or.id/feed",
   },
   {
-    code: "id-almanhaj",
+    code: "ID-ALMANHAJ",
     name: "Almanhaj.or.id",
     url: "https://almanhaj.or.id/feed",
   },
   {
-    code: "id-remaja-islam",
+    code: "ID-REMAJA-ISLAM",
     name: "RemajaIslam.com",
     url: "https://muslim.or.id/feed",
   },
   {
-    code: "id-pengusaha-muslim",
+    code: "ID-PENGUSAHA-MUSLIM",
     name: "PengusahaMuslim.com",
     url: "https://pengusahamuslim.com/feed",
   },
     {
-    code: "id-rumaysho",
+    code: "ID-RUMAYSHO",
     name: "Rumaysho.com",
     url: "https://rumaysho.com/feed",
   },
